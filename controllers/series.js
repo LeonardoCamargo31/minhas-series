ad56cd9de34b43532b1667eff49dc561c45694e4
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2019-05-30 11:23:32 -0300

    Realizando crud em série e organizando a lista de séries

--- controllers/series.js
+++ controllers/series.js
@@ -1,22 +1,51 @@
-const Serie = require('../models/serie')
-
-const index = (req, res) => {
+const index = ({ Serie }, req, res) => {
     Serie.find({}, (err, result) => {
         res.render('series/index', { series: result })
     })
-
 }
 
-const nova = (req, res) => {
-    const serie = new Serie({
-        name: 'Breaking bad',
-        status: 'watched'
+const novaProcess = ({ Serie }, req, res) => {
+    const serie = new Serie(req.body)
+    serie.save(() => {
+        res.redirect('/series')
     })
-    serie.save()
-    res.render('series/index')
+}
+
+const novaForm = (req, res) => {
+    res.render('series/nova')
+}
+
+const excluir = ({ Serie }, req, res) => {
+    const id = req.params.id
+    Serie.remove({
+        _id: id
+    }, (err) => {
+        res.redirect('/series')
+    })
+}
+
+const editarForm = ({ Serie }, req, res) => {
+    Serie.findOne({ _id: req.params.id }, (err, serie) => {
+        res.render('series/editar', { serie })
+    })
+}
+
+const editarProcess = ({ Serie }, req, res) => {
+    const id = req.params.id
+    Serie.findOne({ _id: req.params.id }, (err, serie) => {
+        serie.name = req.body.name
+        serie.status = req.body.status
+        serie.save(() => {
+            res.redirect('/series')
+        })
+    })
 }
 
 module.exports = {
     index,
-    nova
+    novaProcess,
+    novaForm,
+    excluir,
+    editarForm,
+    editarProcess
 }
--- routes/series.js
+++ routes/series.js
@@ -1,9 +1,22 @@
 const seriesController = require('../controllers/series')
 
+const Serie = require('../models/serie')
+const models={
+    Serie
+}
+
 const express = require('express')
 const router = express.Router()
 
-router.get('/', seriesController.index)
-router.get('/nova', seriesController.nova)
+router.get('/', seriesController.index.bind(null,models))
+
+router.get('/nova', seriesController.novaForm)
+
+router.post('/nova', seriesController.novaProcess.bind(null,models))
+
+router.get('/excluir/:id', seriesController.excluir.bind(null,models))
+
+router.get('/editar/:id', seriesController.editarForm.bind(null,models))
+router.post('/editar/:id', seriesController.editarProcess.bind(null,models))
 
 module.exports = router
